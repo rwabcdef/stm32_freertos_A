bf86f7d0c9d7854c637344d288a9fce4c6e31348
Rob Woodhouse <rob.woodhouse@planview.com>
2026-07-09 17:11:12 +0100

    Combined C/C++ app running

---
+++ stm32_freertos_A/Core/Src/lib.cpp
@@ -0,0 +1,14 @@
+/*
+ * lib.cpp
+ *
+ *  Created on: 9 Jul 2026
+ *      Author: RobWoodhouse_930x0e3
+ */
+
+
+#include "lib.hpp"
+
+Car::Car(const int startSpeed)
+{
+	this->speed = startSpeed;
+}
---
+++ stm32_freertos_A/Core/Src/lib.hpp
@@ -0,0 +1,21 @@
+/*
+ * lib.hpp
+ *
+ *  Created on: 9 Jul 2026
+ *      Author: RobWoodhouse_930x0e3
+ */
+
+#ifndef SRC_LIB_HPP_
+#define SRC_LIB_HPP_
+
+class Car {
+	protected:
+		int speed;
+
+	public:
+	Car(const int startSpeed = 0);
+};
+
+
+
+#endif /* SRC_LIB_HPP_ */
