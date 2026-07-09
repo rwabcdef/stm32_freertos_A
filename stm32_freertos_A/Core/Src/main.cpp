5a4f50d9d0df83c00fb2223d8369f47370cbf6d5
Rob Woodhouse <rob.woodhouse@planview.com>
2026-07-09 17:43:04 +0100

    different leds

--- stm32_freertos_A/Core/Src/main.cpp
+++ stm32_freertos_A/Core/Src/main.cpp
@@ -111,6 +111,8 @@ int main(void)
 
   // set green led on
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+  //HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
   
   while (1)
   {
